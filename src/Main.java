7b3f802f358f052c0a8adeb09134e45fdaad99a9
KitSlasher <aniskinnikitos@gmail.com>
2025-04-21 19:59:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int number = 1;
-        while(number <= 50){
-            if(number % 2 == 0){
-                System.out.println(number)
-            }
-            number++;
-        }
-
-        int number1 = 1;
-        int quantity = 0;
-        do{
-            if(number1 % 7 == 0){
-                quantity++;
-            }
-            number++;
-        }while(number <= 100);
-
-        int number2 = 1;
+        int[] massive = {1, -4, 9, 67, -45, 2};
         int sum = 0;
-        while(number2 <= 100){
-            sum = sum + number2;
-            number2++;
+        for(int i = 0; i <= massive.lenght; i++){
+            sum = sum + massive[i];
         }
-        System.out.println(sum);
+        System.out.println(sum / massive.lenght);
 
-        sum1 = 0
-        do{
-            int v = int (Math.random() * 20) + 1;
-            sum = sum + v;
-        }while(v != 15)
+        for(int i = 0; i <= massive.lenght; i++){
+            if(massive[i] < 0){
+                massive[i] = massive[i] * -1
+            }
+        }
     }
 }
