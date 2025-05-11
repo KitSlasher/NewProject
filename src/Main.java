0a31d27cc479ea3272e989ded11f198ed6aecd17
KitSlasher <aniskinnikitos@gmail.com>
2025-05-11 13:49:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,38 @@ public class Main {
 
         for(int i = 0; i <= massive.lenght; i++){
             if(massive[i] < 0){
-                massive[i] = massive[i] * -1
+                massive[i] = massive[i] * (-1);
+            }
+        }
+
+        int [][] massive1 = {{5,7,3,17}, {7,0,1,12}, {8,1,2,3}};
+        int sum1 = 0;
+        for(int i = 0; i <= massive1.lenght; i++){
+            for(int a = 0; a <= massive[i].lenght; a++){
+                sum += massive[i][a];
+            }
+            System.out.println(sum);
+            sum = 0;
+        }
+
+        int maxValue = 0;
+        int indexMax = 0;
+        for(int i = 0; i <= massive.lenght; i++){
+            if(maxValue < massive[i]){
+                maxValue = massive[i];
+            }
+        }
+        for(int i = 0; i <= massive.lenght; i++){
+            if(massive[i] == maxValue){
+                indexMax = i;
+            }
+        }
+
+        for(int i = 0; i <= massive1.lenght; i++){
+            for(int a = 0; a <= massive.lenght; a++){
+                if(massive[a] == massive[i] && a != i){
+                    System.out.println("YES!")
+                }
             }
         }
     }
