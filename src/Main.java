2e42f79b57d4c02d545b1632fc30c70a25076e3a
KitSlasher <aniskinnikitos@gmail.com>
2025-05-21 18:43:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,36 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Animal lion = new Animal();
-        lion.thisAnimal;
+        Car electro1 = new ElectricCar();
+        electro1.startEngine;
+        electro1.stopEngine;
+        electro1.needBatary;
     }
 }
-class Animal{
-    String type;
-    String name;
-    public Animal(String type, String name){
-        this.name = name;
-        this. type = type;
-    public void thisAnimal(String name, String type)P{
-        System.out.println("Это " + type + " по имени " + name)
+class Car{
+    private int year;
+    private String model;
+    private String brand;
+    public String startEngine(){
+        System.out.println("Двигатель запущен");
+    }
+    public String stopEngine(){
+        System.out.println("Двигатель остановлен");
     }
 }
-
-class Library{
-    String[] books = new String[a];
-    public void addBook(String nameBook){
-        books.append(nameBook);
+class ElectricCar extends Car{
+    private int batary;
+    @Override
+    public String startEngine(){
+        System.out.println("Электродвигатель запущен");
     }
-    public void bookss(String[] books){
-        for(String book: books){
-            System.out.println(book);
-        }
+    @Override
+    public String stopEngine(){
+        System.out.println("Электродвигатель остановлен");
     }
-    public void searchBook(String bookz){
-        for(String book: books){
-            if(book == bookz){
-                System.out.println("Книга " + bookz + " найдена");
-            }else{
-                System.out.println("Книга " + bookz + "  не найдена");
-            }
+    public String needBatary(){
+        if(batary < 20){
+            System.out.println("Батарея разряжена, требуется зарядка")
         }
     }
 }
