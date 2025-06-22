d80ae77a6c0e6d5349334f818cd4186f2278851d
KitSlasher <aniskinnikitos@gmail.com>
2025-06-22 15:11:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        Bike bike = new Bike();
-        TransportManager carManager = new TransportManager(car);
-        TransportManager bikeManager = new TransportManager(bike);
-        carManager.startTransport;
-        bikeManager.startTransport;
+        
     }
 }
-public interface Transport{
-    public String move();
-}
-class Car implements Transport{
-    public String move(){
-        System.out.println("Машина пришла в движение");
+class CustomExiption extends Exption{
+    public CustomExiption(String message){
+        super(message);
     }
 }
-class Bike implements Transport{
-    public String move(){
-        System.out.println("Велосипед пришел в движение");
-    }
-}
-class TransportManager{
-    private final Transport transport;
-    public startTransport(Transport transport){
-        transport.move()
+class ExceptionExample{
+    public static void main(String[] args){
+        try{
+            int b = 100;
+            int a = 0;
+            System.out.println(b / a);
+        }catch(ArithmeticExiption e){
+            System.out.println("на ноль делить нельзя");
+        }catch(InputMismathcExiption e){
+            System.out.println("Делить на строку невозможно");
+        }catch(CustomExiption e){
+            System.out.println("Делим только на положительные числа")
+        }
     }
 }
