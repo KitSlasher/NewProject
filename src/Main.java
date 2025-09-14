e8eebd6bf4656d40aad9a9cbce191e334e9cad75
KitSlasher <aniskinnikitos@gmail.com>
2025-09-14 17:06:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,44 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        List<String> list = new Arraylist<>();
+        list.add("Банан");
+        list.add("Яблоко");
+        list.add("Молоко");
+        list.remove(1);
+        list.contains("Кефир")
+    }
+    public List<int> newList(List<int>){
+        Treeset<int> treeSet = new TreeSet<>(List<int>)
     }
 }
-class CustomExiption extends Exption{
-    public CustomExiption(String message){
-        super(message);
+class Task{
+    private String task;
+    private int priority;
+
+    public Task(String task, itn priority){
+        this.task = task;
+        this.priority = priority;
+    }
+    public int getPriority(){
+        return preority;
     }
 }
-class ExceptionExample{
-    public static void main(String[] args){
-        try{
-            int b = 100;
-            int a = 0;
-            System.out.println(b / a);
-        }catch(ArithmeticExiption e){
-            System.out.println("на ноль делить нельзя");
-        }catch(InputMismathcExiption e){
-            System.out.println("Делить на строку невозможно");
-        }catch(CustomExiption e){
-            System.out.println("Делим только на положительные числа")
-        }
+class ListTasks(){
+    private List<Task> listTasks;
+
+    public ListTasks(List<Task> listTasks){
+        this.listTasks = listTasks;
+    }
+    public Task addTask(String task, int priority){
+        listTasks.add(new Task(task, priority));
+        return task;
+    }
+    public Task addTask(String task, int priority){
+        listTasks.remove(priority);
+        return task;
+    }
+    public List<Task> SortByTask(){
+        Collections.sort(listTasks)
     }
 }
